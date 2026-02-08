4f1ad9e640bd48f5d1a508c14c4eea747f45515c
mateusmoutinho <mateusmoutinho01@gmail.com>
2026-02-07 22:03:25 -0300

    att

--- src/src_dependencies/dep_define.dependencies.c
+++ src/src_dependencies/dep_define.dependencies.c
@@ -1,6 +1,6 @@
 //silver_chain_scope_start
 //mannaged by silver chain: https://github.com/OUIsolutions/SilverChain
-#include "../imports/imports.fdefine.h"
+#include "../imports/imports.fdeclare.h"
 //silver_chain_scope_end
 
 #ifdef DEFINE_DEPENDENCIES
