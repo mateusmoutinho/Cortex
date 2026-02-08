79f722bb69557deaa831a89a5cbf58c70a3d32dc
mateusmoutinho <mateusmoutinho01@gmail.com>
2026-02-08 11:40:03 -0300

    at

--- app.c
+++ app.c
@@ -1,15 +1,23 @@
 
-typedef void request;
-typedef void response;
+typedef void apprequest;
+typedef void appresponse;
 
 typedef struct appdeps{
-    const request *request;
+    const apprequest *apprequest;
     const char *route;
-    const char *(*get_headder)(const request *request, const char *key);
-    const char *(*get_query_param)(const request *request, const char *key);
-    const unsigned char *(*read_body)(const request *request, long size);
-    const response *(*send_any)(const unsigned char *content,long content_size,int status_code);
-    const response *(*send_file)(const char *path,int status_code);
+    const char *(*get_headder)(const apprequest *apprequest, const char *key);
+    const char *(*get_headder_key)(const apprequest *apprequest,int index);
+    const char *(*get_headder_value)(const apprequest *apprequest,int index);
+
+
+    const char *(*get_method)(const apprequest *apprequest); 
+    const char *(*get_query_param)(const apprequest *apprequest, const char *key);
+    const char *(*get_query_param_key)(const apprequest *apprequest,int index);
+    const char *(*get_query_param_value)(const apprequest *apprequest,int index);
+   
+    const unsigned char *(*read_body)(const apprequest *apprequest, long size);
+    const appresponse *(*send_any)(const unsigned char *content,long content_size,int status_code);
+    const appresponse *(*send_file)(const char *path,int status_code);
 
 
 
