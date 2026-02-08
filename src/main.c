8e933efa46e6d0c95ae74d7cd80e0ebe8bb83c85
mateusmoutinho <mateusmoutinho01@gmail.com>
2026-02-07 22:00:44 -0300

    att

--- src/main.c
+++ src/main.c
@@ -1,4 +1,10 @@
 
-int main(){
+CwebHttpResponse *main_server(CwebHttpRequest *request) {
+    return cweb_send_text("Hello World", 200);
+}
+
+int main(int argc, char *argv[]) {
+    CwebServer server = newCwebSever(5000, main_server);
+    CwebServer_start(&server);
     return 0;
 }
