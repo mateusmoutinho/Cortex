869717d234f58a8b1f1cd875e9fb71867dd6e86e
mateusmoutinho <mateusmoutinho01@gmail.com>
2026-02-07 21:59:00 -0300

    starting the project

---
+++ src/src_dependencies/dep_declare.dependencies.h
@@ -0,0 +1,6 @@
+
+
+#include "../../dependencies/doTheWorld.h"
+
+#include "../../dependencies/CArgvParse.h"
+#include "../../dependencies/CWebStudio.h"
---
+++ src/src_dependencies/dep_define.dependencies.c
@@ -0,0 +1,12 @@
+//silver_chain_scope_start
+//mannaged by silver chain: https://github.com/OUIsolutions/SilverChain
+#include "../imports/imports.fdefine.h"
+//silver_chain_scope_end
+
+#ifdef DEFINE_DEPENDENCIES
+#include "../../dependencies/CArgvParse.c"
+
+#include "../../dependencies/CTextEngine.c"
+#include "../../dependencies/CWebStudio.c"
+#include "../../dependencies/doTheWorldOne.c"
+
